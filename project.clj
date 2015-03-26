ef7ede685715fd20890c60d0a6821bd7d3f781c4
Ryo Fukumuro <rkworks@o0o.co>
2015-03-27 00:20:15 +0900

    Version 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rkworks/baum "0.1.0"
+(defproject rkworks/baum "0.1.1-SNAPSHOT"
   :description "An extended Clojure reader designed to create self-contained configuration files"
   :url "https://github.com/rkworks/baum"
   :license {:name "Eclipse Public License"
