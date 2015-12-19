740b2a09109c4ff71cc84164bf44d36d961e9a4e
Ryo Fukumuro <rkworks@o0o.co>
2015-12-20 03:18:10 +0900

    Update deps

--- project.clj
+++ project.clj
@@ -10,18 +10,18 @@
                                       :password [:gpg :env]}]
                         ["releases" {:url "https://clojars.org/repo/"
                                      :creds :gpg}]]
-  :dependencies [[environ "1.0.0"]
+  :dependencies [[environ "1.0.1"]
                  [me.raynes/fs "1.4.6"]
                  [org.clojure/clojure "1.7.0"]
                  [org.clojure/core.match "0.3.0-alpha4"]
-                 [org.clojure/tools.reader "0.9.2"]]
-  :profiles {:dev    {:dependencies [[midje "1.7.0"]]
-                      :plugins      [[lein-midje "3.1.3"]
-                                     [lein-environ "1.0.0"]]
+                 [org.clojure/tools.reader "1.0.0-alpha1"]]
+  :profiles {:dev    {:dependencies [[midje "1.8.3"]]
+                      :plugins      [[lein-midje "3.2"]
+                                     [lein-environ "1.0.1"]]
                       :env          {:env "dev"}}
              :1.5    {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6    {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7    {:dependencies [[org.clojure/clojure "1.7.0"]]}
-             :1.8    {:dependencies [[org.clojure/clojure "1.8.0-alpha3"]]}
+             :1.8    {:dependencies [[org.clojure/clojure "1.8.0-RC4"]]}
              :master {:dependencies [[org.clojure/clojure "1.8.0-master-SNAPSHOT"]]}}
   :aliases {"all" ["with-profile" "+1.5:+1.6:+1.7:+1.8:+master"]})
