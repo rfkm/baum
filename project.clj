461f1ba184af5de4cc41c690496793747c0b7a8a
Ryo Fukumuro <rkworks@o0o.co>
2016-01-29 16:40:27 +0900

    Update deps

--- project.clj
+++ project.clj
@@ -2,26 +2,26 @@
   :description "Extensible EDSL in EDN for building self-contained configuration files"
   :url "https://github.com/rfkm/baum"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :jvm-opts ["-XX:-OmitStackTraceInFastThrow"]
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
-  :deploy-repositories [["snapshots" {:url "https://clojars.org/repo/"
+  :deploy-repositories [["snapshots" {:url      "https://clojars.org/repo/"
                                       :username [:gpg :env]
                                       :password [:gpg :env]}]
-                        ["releases" {:url "https://clojars.org/repo/"
+                        ["releases" {:url   "https://clojars.org/repo/"
                                      :creds :gpg}]]
-  :dependencies [[environ "1.0.1"]
+  :dependencies [[environ "1.0.2"]
                  [me.raynes/fs "1.4.6"]
-                 [org.clojure/clojure "1.7.0"]
                  [org.clojure/core.match "0.3.0-alpha4"]
-                 [org.clojure/tools.reader "1.0.0-alpha1"]]
-  :profiles {:dev    {:dependencies [[midje "1.8.3"]]
+                 [org.clojure/tools.reader "1.0.0-alpha2"]]
+  :profiles {:dev    {:dependencies [[org.clojure/clojure "1.8.0"]
+                                     [midje "1.8.3"]]
                       :plugins      [[lein-midje "3.2"]
-                                     [lein-environ "1.0.1"]]
+                                     [lein-environ "1.0.2"]]
                       :env          {:env "dev"}}
              :1.5    {:dependencies [[org.clojure/clojure "1.5.1"]]}
              :1.6    {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7    {:dependencies [[org.clojure/clojure "1.7.0"]]}
-             :1.8    {:dependencies [[org.clojure/clojure "1.8.0-RC4"]]}
-             :master {:dependencies [[org.clojure/clojure "1.8.0-master-SNAPSHOT"]]}}
+             :1.8    {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :master {:dependencies [[org.clojure/clojure "1.9.0-master-SNAPSHOT"]]}}
   :aliases {"all" ["with-profile" "+1.5:+1.6:+1.7:+1.8:+master"]})
