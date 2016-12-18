239f9cf9ce0f0d014ba391694627681fe87b7a6c
Ryo Fukumuro <rfkm@nop.sh>
2016-12-18 18:44:12 +0900

    Version 0.4.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject rkworks/baum "0.4.0"
+(defproject rkworks/baum "0.4.1-SNAPSHOT"
   :description "Extensible EDSL in EDN for building self-contained configuration files"
   :url "https://github.com/rfkm/baum"
   :license {:name "Eclipse Public License"
