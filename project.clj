3112ffce99033f85221de1f9b5b92665674e8051
Ryo Fukumuro <rfkm@nop.sh>
2016-12-18 19:04:05 +0900

    Use the latest version of Midje only with Clojure 1.9

--- project.clj
+++ project.clj
@@ -15,7 +15,7 @@
                  [org.clojure/core.match "0.3.0-alpha4"]
                  [org.clojure/tools.reader "1.0.0-alpha2"]]
   :profiles {:dev    {:dependencies [[org.clojure/clojure "1.8.0"]
-                                     [midje "1.9.0-alpha6"]]
+                                     [midje "1.8.3"]]
                       :plugins      [[lein-midje "3.2.1"]
                                      [lein-environ "1.1.0"]]
                       :env          {:env "dev"}}
@@ -23,5 +23,6 @@
              :1.6    {:dependencies [[org.clojure/clojure "1.6.0"]]}
              :1.7    {:dependencies [[org.clojure/clojure "1.7.0"]]}
              :1.8    {:dependencies [[org.clojure/clojure "1.8.0"]]}
-             :master {:dependencies [[org.clojure/clojure "1.9.0-master-SNAPSHOT"]]}}
+             :master {:dependencies [[org.clojure/clojure "1.9.0-master-SNAPSHOT"]
+                                     [midje "1.9.0-alpha6"]]}}
   :aliases {"all" ["with-profile" "+1.5:+1.6:+1.7:+1.8:+master"]})
